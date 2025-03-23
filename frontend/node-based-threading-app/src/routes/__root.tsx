2608b868137368454f4a0d8ebd012dfbae9547ac
Nazarii Mykhailenko <mykhailenkonazar24@gmail.com>
2025-03-23 12:54:26 +0200

    feat(ui): implement sidebar and improve app structure

--- frontend/node-based-threading-app/src/routes/__root.tsx
+++ frontend/node-based-threading-app/src/routes/__root.tsx
@@ -1,15 +1,15 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import NavBar from '../components/ui/nav-bar'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import NavBar from "../components/ui/nav-bar";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="flex flex-col h-full">
-      <NavBar />
+    <div className="flex h-full flex-col">
+      {/* <NavBar /> */}
       <div className="flex-1 overflow-hidden">
         <Outlet />
       </div>
       <TanStackRouterDevtools />
     </div>
   ),
-})
+});
--- frontend/node-based-threading-app/src/routes/index.tsx
+++ frontend/node-based-threading-app/src/routes/index.tsx
@@ -1,16 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
-import '@xyflow/react/dist/style.css';
+import { createFileRoute } from "@tanstack/react-router";
+import "@xyflow/react/dist/style.css";
 
-export const Route = createFileRoute('/')({
+import { Flow } from "../components/Flow";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { ReactFlowProvider } from "@xyflow/react";
+import { DnDProvider } from "@/lib/DnDContext";
+
+export const Route = createFileRoute("/")({
   component: Index,
-})
-
-import { Flow } from '../components/Flow';
+});
 
 function Index() {
   return (
-    <div className="w-full h-full">
-      <Flow />
-    </div>
+    <SidebarProvider>
+      <DnDProvider>
+        <div className="flex h-screen w-full overflow-hidden">
+          <div className="flex-shrink-0">
+            <AppSidebar />
+          </div>
+          <div className="relative flex flex-1">
+            {/* Flow area */}
+            <div className="relative flex-1">
+              <div className="absolute top-2 left-2 z-20">
+                <SidebarTrigger />
+              </div>
+              <div className="h-full w-full">
+                <ReactFlowProvider>
+                  <Flow />
+                </ReactFlowProvider>
+              </div>
+            </div>
+          </div>
+        </div>
+      </DnDProvider>
+    </SidebarProvider>
   );
 }
