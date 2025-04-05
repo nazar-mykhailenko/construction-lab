242dfa067e47f983debb5ef64532fff39a477d1a
Nazarii Mykhailenko <mykhailenkonazar24@gmail.com>
2025-04-05 14:49:11 +0300

    feat: add CORS policy to allow all origins

--- backend/NodeBasedThreading.API/Program.cs
+++ backend/NodeBasedThreading.API/Program.cs
@@ -2,6 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 builder.Services.AddControllers();
 builder.Services.AddRouting(opt =>
 {
@@ -11,6 +20,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowAllOrigins");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
